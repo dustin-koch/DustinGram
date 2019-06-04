a349890f62ffe0b756d588e8b6cc6ae60013545c
Dustin Koch <dustin.koch@me.com>
2019-06-04 14:18:26 -0600

    models and controller complete

---
+++ Continuum/Models/Comment.swift
@@ -0,0 +1,23 @@
+//
+//  Comment.swift
+//  Continuum
+//
+//  Created by Dustin Koch on 6/4/19.
+//  Copyright © 2019 Rabbit Hole Fashion. All rights reserved.
+//
+
+import Foundation
+
+class Comment {
+    var text: String
+    var timestamp: Date
+    weak var post: Post?
+    
+    init(text: String, timestamp: Date = Date(), post: Post?) {
+        self.text = text
+        self.timestamp = timestamp
+        self.post = post
+    }
+    
+    
+}//END OF CLASS
---
+++ Continuum/Models/Post.swift
@@ -0,0 +1,34 @@
+//
+//  Post.swift
+//  Continuum
+//
+//  Created by Dustin Koch on 6/4/19.
+//  Copyright © 2019 Rabbit Hole Fashion. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Post {
+    var photoData: Data?
+    var timestamp: Date
+    var caption: String
+    var comments: [Comment]
+    var photo: UIImage? {
+        get {
+            guard let photoData = photoData else { return nil }
+            return UIImage(data: photoData)
+        }
+        set {
+            photoData = newValue?.jpegData(compressionQuality: 0.5)
+        }
+    }
+    
+    init(timestamp: Date = Date(), caption: String, comments: [Comment] = [], photo: UIImage?) {
+        self.timestamp = timestamp
+        self.caption = caption
+        self.comments = comments
+        self.photo = photo
+    }
+    
+}//END OF CLASS
